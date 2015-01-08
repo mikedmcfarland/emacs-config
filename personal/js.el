5811a15b2736c6aeeb4b6ebc123bd58459b14586
Michael McFarland <mikedmcfarland@gmail.com>
2015-01-08 15:51:13 -0500

    added tern to js config

--- personal/js.el
+++ personal/js.el
@@ -1,3 +1,11 @@
+;;Set up tern js completion/refactoring
+(prelude-require-package 'tern)
+(add-hook 'prelude-js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (prelude-require-package 'tern-auto-complete)
+     (tern-ac-setup)))
+
 ;;Set tab width of javascript to be 2 spaces
 (setq-default js-indent-level 2)
 (setq-default js2-basic-offset 2)
